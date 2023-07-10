89156c1224c82b189aa078dba5e26741287ec62b
izumu07 <izumu07@outlook.com>
2023-07-10 13:03:15 +0000

    test

--- mySoC/PC.v
+++ mySoC/PC.v
@@ -7,11 +7,26 @@ input clk_i,
 input rst_i,
 output  reg[31:0] pc
 );
-always@(posedge clk_i or posedge rst_i)
+
+reg temp_rst;
+
+always@(posedge clk_i or posedge rst_i )
+begin
+    if (rst_i)
+        temp_rst<=0;
+    else if (temp_rst==0)
+        temp_rst<=1;
+    else
+        temp_rst<=1;
+end
+
+always@(posedge clk_i or posedge rst_i )
     begin
         if (rst_i)
             pc<=0;
-        else
+        else if(temp_rst)
             pc<=din;
+        else
+            pc<=pc;
     end
 endmodule
--- mySoC/RF.v
+++ mySoC/RF.v
@@ -1,61 +1,61 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 2023/07/03 20:08:09
-// Design Name: 
+// Design Name:
 // Module Name: RF
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 `include "defines.vh"
 
 
 module RF(
-input [4:0]rR1,
-input [4:0]rR2,
-input [4:0]wR,
-input [31:0]wD,
-input clk_i,
-input rst_i,
-input op,
-output reg [31:0]rD1,
-output reg [31:0]rD2
-);
-reg [31:0]rfs[31:1];
-integer k;
-always@(*)
-begin
-if(rR1==32'b0)
-rD1=32'b0;
-else
-rD1=rfs[rR1];
-if(rR2==32'b0)
-rD2=32'b0;
-else
-rD2=rfs[rR2];
-end
+    input [4:0]rR1,
+    input [4:0]rR2,
+    input [4:0]wR,
+    input [31:0]wD,
+    input clk_i,
+    input rst_i,
+    input op,
+    output reg [31:0]rD1,
+    output reg [31:0]rD2
+  );
+  reg [31:0]rfs[31:1];
+  integer k;
+  always@(*)
+  begin
+    if(rR1==32'b0)
+      rD1=32'b0;
+    else
+      rD1=rfs[rR1];
+    if(rR2==32'b0)
+      rD2=32'b0;
+    else
+      rD2=rfs[rR2];
+  end
 
-always@(posedge clk_i)
-begin
-if(rst_i)
-for (k=1;k<32;k=k+1)
-rfs[k]=32'b0;
-else
-begin
-if(op)
-rfs[wR]=wD;
-end
-end
+  always@(posedge clk_i  or posedge rst_i)
+  begin
+    if(rst_i)
+      for (k=1;k<32;k=k+1)
+        rfs[k]<=32'b0;
+    else if(wR!=32'h0)
+    begin
+      if(op)
+        rfs[wR]<=wD;
+    end
+  end
 
 endmodule
